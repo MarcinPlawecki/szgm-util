98bad6730bdc89bb26c53ab81580138c0b01a666
MarcinPlawecki <marcin.plawecki89@gmail.com>
2013-12-23 19:59:25 +0100

    initial commit

---
+++ src/main/java/collections/CollectionUtils.java
@@ -0,0 +1,12 @@
+package collections;
+
+import java.util.Collection;
+
+import oracle.net.aso.e;
+
+public class CollectionUtils {
+
+	public static boolean isNotEmpty(Collection<e> col) {
+		return null != col && false == col.isEmpty();
+	}
+}
